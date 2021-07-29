25e95fe5f9e3375138ed239be19aa20c5db37992
danoxv <65130664+danoxv@users.noreply.github.com>
2021-07-29 19:04:21 +0300

    Update 9.php

--- 9.php
+++ 9.php
@@ -1,40 +1,63 @@
 <?php
+
+//Задача 9.1
+//Сделайте класс Employee, в котором будут следующие свойства: name (имя), surname (фамилия) и salary (зарплата).
+
 class Employee {
 
-    private $name;
-    private $surname;
-    private $salary;
+    private string $name;
+    private string $surname;
+    private int $salary;
 
-    public function __construct($name, $surname)
+    /**
+     * 
+     * @param string $name
+     * @param string $surname
+     */
+    // Задача 9.2
+    // Сделайте так, чтобы свойства name и surname были доступны только для чтения, а свойство salary - и для чтения, и для записи.
+    public function __construct(string $name, string $surname)
     {
         $this->name = $name;
         $this->surnaname = $surname;
     }
 
-    public function getName()
+    /**
+     * 
+     * @return string
+     */
+    public function getName(): string
     {
         return $this->name;
     }
 
-    public function getsurname()
+    /**
+     * 
+     * @return int
+     */
+    public function getsurname(): int
     {
         return $this->age;
     }
 
-    public function setSalary($salary)
+    /**
+     * 
+     * @param int $salary
+     */
+    public function setSalary(int $salary)
     {
         $this->salary = $salary . '$';
     }
 
-     public function getsalary()
+    /**
+     * 
+     * @return int
+     */
+    public function getsalary(): int
     {
         return $this->salary;
     }
-    
+
 }
 
-$user = new Employee('gsfdddddddddddddd111111111111111111111tnz','fdgnsgbfshn');
-echo $user->getName();
-$user->setSalary("500");
-echo $user->getSalary();
 ?>
