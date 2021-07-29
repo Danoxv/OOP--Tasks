2ebca726fbce545cf56725a1727f7e9988b9148c
danoxv <65130664+danoxv@users.noreply.github.com>
2021-07-29 22:22:06 +0300

    Update 13.1-2.php

--- 13.1-2.php
+++ 13.1-2.php
@@ -1,10 +1,18 @@
 <?php
 
+// Задача 13.1
+// Реализуйте класс Arr, похожий на тот, который я реализовал выше.
+// В отличие от моего класса метод add вашего класса параметром должен принимать массив чисел.
+// Все числа из этого массива должны добавляться в конец массива $this->numbers.
 class Arr {
 
-    private $numbers = [];
+    /**
+     * 
+     * @var int[]
+     */
+    private array $numbers = [];
 
-    public function add(array $numbers)
+    public function add(array $numbers): void
     {
         foreach ($numbers as $number) {
             if (!in_array($number, $this->numbers)) {
@@ -13,10 +21,18 @@ class Arr {
         }
     }
 
-    public function getAvg()
+//Задача 13.2
+//Вместо метода getSum реализуйте метод getAvg, который будет находить среднее арифметическое переданных чисел.
+
+    /**
+     * 
+     * @return int|float
+     */
+    public function getAvg(): int|float
     {
-        return array_sum($this->numbers)/count($this->numbers);
+        return array_sum($this->numbers) / count($this->numbers);
     }
+
 }
 
 $numbers = [1, 2, 3, 4, 5];
