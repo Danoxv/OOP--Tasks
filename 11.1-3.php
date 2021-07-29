453879bfe0db45113cb293c56a221fd32173f740
danoxv <65130664+danoxv@users.noreply.github.com>
2021-07-29 21:15:47 +0300

    Update 11.1-3.php

--- 11.1-3.php
+++ 11.1-3.php
@@ -2,11 +2,11 @@
 
 //Задача 11.1
 //Сделайте класс City (город), в котором будут следующие свойства: name (название города), population (количество населения).
-        
+
 class City {
 
     public string $name;
-    public int|float $population;
+    public int $population;
 
     public function __construct($name, $population)
     {
@@ -15,9 +15,10 @@ class City {
     }
 
 }
-$arr = [new City('belgorod', 1),new City ('kharkov',)];
+
+$arr = [new City('belgorod', 1), new City('kharkov',)];
 foreach ($arr as $value) {
- echo $value->name . $value->population ;
+    echo $value->name . $value->population;
 }
 
 ?>
