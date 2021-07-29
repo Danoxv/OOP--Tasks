37adc61b95e12991af7e8b22eb838f7c9c8c6799
danoxv <65130664+danoxv@users.noreply.github.com>
2021-07-30 00:42:55 +0300

    Update 17.1-3.php

--- 17.1-3.php
+++ 17.1-3.php
@@ -1,21 +1,31 @@
 <?php
 
-/*
- * this class presents  the tasks 17.1-2
- * 
- * 
- */
+// Задача 17.1
+// Не подсматривая в мой код самостоятельно реализуйте такой же класс Arr, методы которого будут вызываться в виде цепочки.
 
 class Arr {
 
-    public $numbers = [];
+    public array $numbers = [];
 
-    public function add($numbers)
+    /**
+     * 
+     * @param int $number
+     * @return $this
+     */
+    public function add(int $number)
     {
-        $this->numbers[] = $numbers;
+        $this->numbers[] = $number;
         return $this;
     }
 
+    //Задача 17.2
+//Добавьте в класс Arr еще один метод append, который параметром будет принимать массив чисел и добавлять эти числа в конец массива, хранящегося в объекте.
+
+    /**
+     * 
+     * @param array $numbers
+     * @return $this
+     */
     public function append(array $numbers)
     {
         foreach ($numbers as $number) {
@@ -23,8 +33,11 @@ class Arr {
         }
         return $this;
     }
-
-    public function getSum()
+/**
+ * 
+ * @return int|float
+ */
+    public function getSum(): int|float
     {
         return array_sum($this->numbers);
     }
@@ -33,31 +46,43 @@ class Arr {
 
 echo (new Arr)->add(1)->append([1, 2])->add(2)->getsum();
 
-
-/*
- * this class presents the task 17.3
- * 
- */
+//Задача 17.3
+//Сделайте класс User, у которого будут приватные свойства surname (фамилия), name (имя) и patronymic (отчество).
+//Эти свойства должны задаваться с помощью соответствующих сеттеров.
+//Сделайте так, чтобы эти сеттеры вызывались цепочкой в любом порядке, а самым последним методом в цепочке можно было вызвать метод getFullName,
+//который вернет ФИО юзера (первую букву фамилии, имени и отчества).
 
 class User {
 
-    private $surname;
-    private $name;
-    private $patronymic;
-
-    public function setSurname($Surname)
+    private string $surname;
+    private string $name;
+    private string $patronymic;
+/**
+ * 
+ * @param string $Surname
+ * @return $this
+ */
+    public function setSurname(string$Surname)
     {
         $this->surname = $Surname;
         return $this;
     }
-
-    public function setName($Name)
+/**
+ * 
+ * @param string $Name
+ * @return $this
+ */
+    public function setName(string$Name)
     {
         $this->name = $Name;
         return $this;
     }
-
-    public function setPatronymic($Patronymic)
+/**
+ * 
+ * @param string $Patronymic
+ * @return $this
+ */
+    public function setPatronymic(string$Patronymic)
     {
         $this->patronymic = $Patronymic;
         return $this;
@@ -67,9 +92,9 @@ class User {
     {
         printf(
                 ' %s%s%s',
-                mb_substr($this->surname, 0, 1) ,
+                mb_substr($this->surname, 0, 1),
                 mb_substr($this->name, 0, 1),
-        mb_substr($this->patronymic, 0, 1)
+                mb_substr($this->patronymic, 0, 1)
         );
     }
 
@@ -77,6 +102,4 @@ class User {
 
 $User = new User();
 echo $User->setSurname('Петр')->setName('Николай')->setPatronymic('Иванович')->getFullName();
-
-
 ?>
